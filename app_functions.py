62da9f6c85e0559da4fc2e7913d7a9472e1f566a
Animedu-admin <general.night.shadow@gmail.com>
2026-01-24 16:34:54 +0000

    Database Changes10

--- app_functions.py
+++ app_functions.py
@@ -8,7 +8,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('Postgres.DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
